8647dd6eea61c133f96f7a5413dfc39f15a5516e
imac <nilmani.j@elaunchinfotech.in>
2022-01-03 17:33:39 +0530

    For Query PRaticeing

--- src/main/kotlin/com/nilmani/jpqlprojectexample/repository/TeacherRepository.kt
+++ src/main/kotlin/com/nilmani/jpqlprojectexample/repository/TeacherRepository.kt
@@ -1,7 +1,6 @@
 package com.nilmani.jpqlprojectexample.repository
 
 import com.nilmani.jpqlprojectexample.entity.Teacher
-import org.hibernate.query.NativeQuery
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
